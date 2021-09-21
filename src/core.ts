7b625dd76bd2d4b026859605ff468e511b84ce01
selfonnector <89555648+selfonnector@users.noreply.github.com>
2021-09-22 06:39:13 +0900

    feat: Make the validation target type flexible

--- src/core.ts
+++ src/core.ts
@@ -1,2 +1,2 @@
-export type Validation<T> = (target: any) => target is T
+export type Validation<T extends TARGET, TARGET = any> = (target: TARGET) => target is T
 export type ValidType<V extends Validation<any>> = V extends Validation<infer T> ? T : never
